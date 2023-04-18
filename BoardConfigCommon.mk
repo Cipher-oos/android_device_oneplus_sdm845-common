371cd4c47d4ad8c41dd59e5cc0cfc7a20dc73eb1
Cipher-oos <ma7moudmo7med628@outlook.sa>
2023-04-18 08:31:32 +0000

    Banana Bringing up

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -78,7 +78,7 @@ TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
 TARGET_SYSTEM_EXT_PROP += $(COMMON_PATH)/system_ext.prop
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
-TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
+#TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
 # Treble
 BOARD_VNDK_VERSION := current
 
